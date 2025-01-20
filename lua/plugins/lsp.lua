cee2c6bb6577243a2f34fa989a9b89e13b222eb2
martin <t4pz0r@gmail.com>
2025-01-20 11:00:16 +0100

    add local installed zls and zig to lsp config and disable mason

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,29 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        zls = {
+          mason = false,
+          path = "/home/tap/zig/current",
+          Zls = {
+            path = "/home/tap/zls/current",
+            enableAutofix = true,
+            enable_snippets = true,
+            enable_ast_check_diagnostics = true,
+            enable_autofix = true,
+            enable_import_embedfile_argument_completions = true,
+            warn_style = true,
+            enable_semantic_tokens = true,
+            enable_inlay_hints = true,
+            inlay_hints_hide_redundant_param_names = true,
+            inlay_hints_hide_redundant_param_names_last_token = true,
+            operator_completions = true,
+            include_at_in_builtins = true,
+            max_detail_length = 1048576,
+          },
+        },
+      },
+    },
+  },
+}
